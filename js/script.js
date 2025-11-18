92a6338a2ba3acbe3b85da1313936fe2fe3fbce7
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-18 16:49:10 +0100

    feat:Aggiunta della barra di caricamento

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 const cardPrint = document.getElementById("card-container");
-
-axios.get("https://lanciweb.github.io/demo/api/pictures/")
-    .then((resp) => {
-        const cardArray = resp.data;
-        console.log(cardArray);
-        let cardOutput = "";
-        cardArray.forEach(function (cardElem) {
-            cardOutput += `
+//Progress bar
+const barContainer = document.querySelector(".bar");
+const progressBar = document.querySelector(".progress-bar");
+let progress = 0;
+let intervalId = null;
+function startProgressBar() {
+    if (intervalId === null) {
+        intervalId = setInterval(function () {
+            if (progress === 100) {
+                clearInterval(intervalId);
+                progress = 0;
+                intervalId = null;
+                barContainer.style.display = "none";
+                //stampa cards
+                axios.get("https://lanciweb.github.io/demo/api/pictures/")
+                    .then((resp) => {
+                        const cardArray = resp.data;
+                        console.log(cardArray);
+                        let cardOutput = "";
+                        cardArray.forEach(function (cardElem) {
+                            cardOutput += `
             <div class="col">
                     <div class="card">
                         <div class="card-image"><img src="${cardElem.url}" />
                             <figcaption>${cardElem.date}<br> <span class="text-title-card"><strong>${cardElem.title}</strong></span></figcaption>
+                            <span class="pin"><img src="../img/pin.svg" />
                         </div>
                     </div>
                 </div>
             `
-            console.log(cardElem.url);
-        });
-        cardPrint.innerHTML = cardOutput;
+                            console.log(cardElem.url);
+                        });
+                        cardPrint.innerHTML = cardOutput;
 
 
-    });
+                    });
+            } else {
+                progress++;
+                progressBar.style.width = progress + "%";
+                progressBar.innerHTML = progress + "%";
+            }
+        }, 50);
+    }
+}
+startProgressBar();
+
